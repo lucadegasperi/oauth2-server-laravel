a06df295c118b21efc35c899c01313d59989d982
Luca Degasperi <me@lucadegasperi.com>
2013-10-06 11:38:56 +0200

    ...

--- tests/OAuthFilterTest.php
+++ tests/OAuthFilterTest.php
@@ -4,6 +4,9 @@ use Mockery as m;
 
 class OAuthFilterTest extends PHPUnit_Framework_TestCase {
 
-
+    public function test_filter_with_no_scope()
+    {
+        $this->assertTrue(true);
+    }
 
 }

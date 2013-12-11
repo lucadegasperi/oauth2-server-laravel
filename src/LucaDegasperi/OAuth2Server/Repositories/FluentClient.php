3ad0ad1684c6247d92f762480dbc875284085788
Luca Degasperi <luca@lucadegasperi.com>
2013-12-11 14:56:17 +0100

    Documentation Fix

--- src/LucaDegasperi/OAuth2Server/Repositories/FluentClient.php
+++ src/LucaDegasperi/OAuth2Server/Repositories/FluentClient.php
@@ -21,6 +21,49 @@ class FluentClient implements ClientInterface
         $this->limitClientsToGrants = $limit;
     }
 
+    /**
+     * Validate a client
+     *
+     * Example SQL query:
+     *
+     * <code>
+     * # Client ID + redirect URI
+     * SELECT oauth_clients.id, oauth_clients.secret, oauth_client_endpoints.redirect_uri, oauth_clients.name,
+     * oauth_clients.auto_approve
+     *  FROM oauth_clients LEFT JOIN oauth_client_endpoints ON oauth_client_endpoints.client_id = oauth_clients.id
+     *  WHERE oauth_clients.id = :clientId AND oauth_client_endpoints.redirect_uri = :redirectUri
+     *
+     * # Client ID + client secret
+     * SELECT oauth_clients.id, oauth_clients.secret, oauth_clients.name, oauth_clients.auto_approve FROM oauth_clients 
+     * WHERE oauth_clients.id = :clientId AND oauth_clients.secret = :clientSecret
+     *
+     * # Client ID + client secret + redirect URI
+     * SELECT oauth_clients.id, oauth_clients.secret, oauth_client_endpoints.redirect_uri, oauth_clients.name,
+     * oauth_clients.auto_approve FROM oauth_clients LEFT JOIN oauth_client_endpoints 
+     * ON oauth_client_endpoints.client_id = oauth_clients.id
+     * WHERE oauth_clients.id = :clientId AND oauth_clients.secret = :clientSecret AND
+     * oauth_client_endpoints.redirect_uri = :redirectUri
+     * </code>
+     *
+     * Response:
+     *
+     * <code>
+     * Array
+     * (
+     *     [client_id] => (string) The client ID
+     *     [client secret] => (string) The client secret
+     *     [redirect_uri] => (string) The redirect URI used in this request
+     *     [name] => (string) The name of the client
+     *     [auto_approve] => (bool) Whether the client should auto approve
+     * )
+     * </code>
+     *
+     * @param  string     $clientId     The client's ID
+     * @param  string     $clientSecret The client's secret (default = "null")
+     * @param  string     $redirectUri  The client's redirect URI (default = "null")
+     * @param  string     $grantType    The grant type used in the request (default = "null")
+     * @return bool|array               Returns false if the validation fails, array on success
+     */
     public function getClient($clientId, $clientSecret = null, $redirectUri = null, $grantType = null)
     {
         $query = null;

f47ff1bea132d228ba8340b75042e207995e6757
Luca <Luca@MacBook-Pro-Luca.local>
2014-04-24 12:40:20 +0200

    yet another migration

---
+++ migrations/2014_04_24_122920_create_oauth_refresh_token_scopes_table.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateOauthRefreshTokenScopesTable extends Migration
+{
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('oauth_refresh_token_scopes', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('refresh_token_id', 40);
+            $table->string('scope_id', 40);
+
+            $table->timestamps();
+
+            $table->index('refresh_token_id');
+            $table->index('scope_id');
+
+            $table->foreign('refresh_token_id')
+                ->references('id')->on('oauth_refresh_tokens')
+                ->onDelete('cascade');
+
+            $table->foreign('scope_id')
+                ->references('id')->on('oauth_scopes')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('oauth_refresh_token_scopes', function (Blueprint $table) {
+            $table->dropForeign('oauth_access_token_scopes_scope_id_foreign');
+            $table->dropForeign('oauth_access_token_scopes_refresh_token_id_foreign');
+        });
+        Schema::drop('oauth_refresh_token_scopes');
+    }
+}

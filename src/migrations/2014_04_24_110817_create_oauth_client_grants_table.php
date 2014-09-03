e06aec671fdca723c5d48436aad54ce8ae3053f9
Christopher L Bray <chris@christopherbray.co.uk>
2014-09-03 10:41:24 +0100

    Specify database connection

--- src/migrations/2014_04_24_110817_create_oauth_client_grants_table.php
+++ src/migrations/2014_04_24_110817_create_oauth_client_grants_table.php
@@ -13,7 +13,9 @@ class CreateOauthClientGrantsTable extends Migration
      */
     public function up()
     {
-        Schema::create('oauth_client_grants', function (Blueprint $table) {
+        $connection = Config::get('oauth2-server-laravel::oauth2.database');
+
+        Schema::connection($connection)->create('oauth_client_grants', function (Blueprint $table) {
             $table->increments('id');
             $table->string('client_id', 40);
             $table->string('grant_id', 40);

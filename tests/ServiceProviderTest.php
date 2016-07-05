f80d19efba9b140eeeae3d8bf5231eaa2812d3bf
Vincent Klaiber <vincentklaiber@gmail.com>
2016-07-05 14:56:46 +0200

    Add service provider test

---
+++ tests/ServiceProviderTest.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ * This file is part of OAuth 2.0 Laravel.
+ *
+ * (c) Luca Degasperi <packages@lucadegasperi.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace LucaDegasperi\OAuth2Server\Tests;
+
+use GrahamCampbell\TestBenchCore\ServiceProviderTrait;
+
+/**
+ * This is the service provider test class.
+ *
+ * @author Vincent Klaiber <hello@vinkla.com>
+ */
+class ServiceProviderTest extends AbstractTestCase
+{
+    use ServiceProviderTrait;
+}
